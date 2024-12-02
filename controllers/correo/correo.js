0635fcbf14006d76ab3055681d64df819330959a
perosnal dev <dev.ti@grupoelcerezo.com>
2024-12-01 20:59:21 -0600

    se agergo endpoint de correo para volkswagen

--- controllers/correo/correo.js
+++ controllers/correo/correo.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const sendEmail = (req, res) => {
-    const { toEmail, fromEmail, text, html } = req.body;
+    const { toEmail, fromEmail, html } = req.body;
   console.log(toEmail,fromEmail,html)
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -15,7 +15,7 @@ const sendEmail = (req, res) => {
     const mailOptions = {
         from: fromEmail,
         to: toEmail,
-        subject: text,
+        subject: 'Hola te saludamos de Regional Artist x-OMODA',
         html: html
     };
 
@@ -29,4 +29,34 @@ const sendEmail = (req, res) => {
     });
 };
 
-module.exports = { sendEmail };
+const sendEmailvw = (req, res) => {
+    const { toEmail, fromEmail, html } = req.body;
+  console.log(toEmail,fromEmail,html)
+    const transportervw = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAILVW,
+            pass: process.env.PASSWORDVW
+        }
+    });
+
+    const mailOptions = {
+        from: fromEmail,
+        to: toEmail,
+        subject: 'Hola te saludamos de un dia para el futuro de volkswagen',
+        html: html
+    };
+
+    transportervw.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).send(error.toString());
+        }
+        console.log('Correo enviado: ' + info.response);
+        res.json('CORRECTO');
+    });
+};
+
+module.exports = { sendEmail
+    ,sendEmailvw
+ };
--- routes/regionalartist/correo.routes.js
+++ routes/regionalartist/correo.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const  sendEmail  = require('../../controllers/correo/correo');
 
 router.post('/sendEmail', sendEmail.sendEmail);
+router.post('/sendEmailvw', sendEmail.sendEmailvw);
 
 module.exports = router;
